708b7a288a7ca1f9acc6020689fc119c6e65fb97
Andre Carvalho <andre.filipe.carvalho@outlook.pt>
2023-04-05 13:48:31 +0100

    .

--- src/Pages/Estadias/estadias.js
+++ src/Pages/Estadias/estadias.js
@@ -1,5 +1,13 @@
 function lermais(){
-    var pontos = document.getElementById("pontos");
-    var maisTexto=document.getElementById("pontos");
-    var 
+    var pontos = document.getElementByClass("pontos");
+    var maisTexto=document.getElementByClass("mais");
+    var btnLerMais=document.getElementByClass("Read_More");
+    if(pontos.style.display==="none"){
+        pontos.style.display="inline";
+        maisTexto.style.display
+    }else{
+        pontos.style.display="none";
+        maisTexto.style.display="inline";
+        btnLerMais.innerHTML="Ler Menos";
+    }
 }
