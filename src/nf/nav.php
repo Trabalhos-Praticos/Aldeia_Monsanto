dfcab75bbd33cc5df1ce149f4b3885ffee31009b
Lucas Cruz <lucas.2001.cruz@gmail.com>
2023-06-13 18:30:21 +0100

    dhfgjdgfhj

--- src/nf/nav.php
+++ src/nf/nav.php
@@ -1,95 +1,18 @@
-
-<!-- Navbar -->
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <!-- Container wrapper -->
+<nav class="navbar navbar-default">
   <div class="container-fluid">
-    <!-- Toggle button -->
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-mdb-toggle="collapse"
-      data-mdb-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <i class="fas fa-bars"></i>
-    </button>
-
-    <!-- Collapsible wrapper -->
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <!-- Navbar brand -->
-      <a class="navbar-brand mt-2 mt-lg-0" href="../../../index.php">
-        <img
-          src="https://iili.io/H6zuJ4e.webp"
-          height="20"
-          alt="MDB Logo"
-          loading="lazy"
-        />
-      </a>
-      <!-- Left links -->
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link" href="src\Pages\Estadias\Estadias.php">Estadias</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="..\..\Pages\Historia\Historia.php">Historia</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="..\..\Pages\ProgramasTuristicos\ProgramasTuristicos.php">Programas Turisticos</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="..\..\Pages\Visitar\Visitar.php">Visitar</a>
-        </li>
-      </ul>
-      <!-- Left links -->
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#index.php">WebSiteName</a>
     </div>
-    <!-- Collapsible wrapper -->
-
-    <!-- Right elements -->
-    <div class="d-flex align-items-center">
-      <!-- Icon -->
-      <a class="text-reset me-3" href="#">
-        <i class="fas fa-shopping-cart"></i>
-      </a>
-      </div>
-      <ul class="navbar-nav">
-      <!-- Avatar -->
-      <li class="nav-item dropdown">
-        <a
-          class="nav-link dropdown-toggle d-flex align-items-center"
-          href="#"
-          id="navbarDropdownMenuLink"
-          role="button"
-          data-mdb-toggle="dropdown"
-          aria-expanded="false"
-        >
-          <img
-            src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img (31).webp"
-            class="rounded-circle"
-            height="22"
-            alt="Portrait of a Woman"
-            loading="lazy"
-          />
-        </a>
-        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <li>
-            <a class="dropdown-item" href="#">My profile</a>
-          </li>
-          <li>
-            <a class="dropdown-item" href="#">Settings</a>
-          </li>
-          <li>
-            <a class="dropdown-item" href="#">Logout</a>
-          </li>
-        </ul>
-      </li>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#index.php">Home</a></li>
+      <li><a href="#Estadias.php">Estadias</a></li>
+      <li><a href="#Historia.php">História</a></li>
+      <li><a href="#ProgramasTuristicos.php">Programas Turísticos</a></li>
+      <li><a href="#Visitar.php">Visitar</a></li>
     </ul>
-    </div>
-    </div>
-    <!-- Right elements -->
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
   </div>
-  <!-- Container wrapper -->
-</nav>
-<!-- Navbar -->
+</nav> 
