9ad49fb4d7595add3f5f0ea9d0a41c73b01bf03a
ludvikcruz <lucas.2001.cruz@gmail.com>
2023-06-14 19:37:03 +0100

    fgjsfgj

--- src/Pages/ProgramasTuristicos/ProgramasTuristicos.php
+++ src/Pages/ProgramasTuristicos/ProgramasTuristicos.php
@@ -10,7 +10,7 @@ require_once __DIR__ . '/../../middleware/middleware-utilizador.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="ProgramasTurísticas.css">
+    <!-- <link rel="stylesheet" href="ProgramasTurísticas.css"> -->
     <title>Programas Turísticos</title>
 </head>
 <body>
@@ -26,7 +26,195 @@ include_once __DIR__ . '../../nf/nav.php';
 
     </div>
 
-    <div class="container_prog">
+
+
+    <div class="card" style="width: 18rem;">
+     <div id="images"><img src="../../Assets/img/biketour1.jpg" alt="" width="300px" height="100px"></div>
+  <div class="card-body">
+    <h5 class="card-title">BIKE TOUR</h5>
+    <a href="#" class="btn btn-primary">Ver mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+ <div id="images"><img src="../../Assets/img/biketour2.jpg" alt="" width="300px" height="100px"></div>
+  <div class="card-body">
+    <h5 class="card-title">PASSEIO DE BICICLETA NAS ALDEIAS HISTÓRICAS PORTUGUESAS</h5>
+    <a href="#" class="btn btn-primary">Ver mais</a>
+  </div>
+</div>
+
+
+<div class="card" style="width: 18rem;">
+<div id="images"><img src="../../Assets/img/caminhada1.jpg" alt="" width="300px" height="100px"></div>
+  <div class="card-body">
+    <h5 class="card-title">CAMINHADA PELAS ALDEIAS HISTÓRICAS – MONSANTO</h5>
+    <a href="#" class="btn btn-primary">Ver mais</a>
+  </div>
+</div>
+
+
+<div class="card" style="width: 18rem;">
+<div id="images"><img src="../../Assets/img/biketour3.jpg" alt="" width="300px" height="100px"></div>
+  <div class="card-body">
+    <h5 class="card-title">CICLOTURISMO NAS ALDEIAS HISTÓRICAS – CASTELO RODRIGO</h5>
+    <a href="#" class="btn btn-primary">Ver mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+<div id="images"><img src="../../Assets/img/biketour2.jpg" alt="" width="300px" height="100px"></div>
+  <div class="card-body">
+    <h5 class="card-title">CICLOTURISMO NAS ALDEIAS HISTÓRICAS – MONSANTO</h5>
+    <a href="#" class="btn btn-primary">Ver mais</a>
+  </div>
+</div>
+
+
+<div class="card" style="width: 18rem;">
+<div id="images"><img src="../../Assets/img/ilhapedra.jpg" alt="" width="300px" height="100px"></div> 
+  <div class="card-body">
+    <h5 class="card-title">DA ILHA DA PEDRA À EGITÂNIA</h5>
+    <a href="#" class="btn btn-primary">Ver mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+<div id="images"><img src="../../Assets/img/sagradoaoprofano.jpg" alt="" width="300px" height="100px"></div>
+  <div class="card-body">
+<h5 class="card-title">DO SAGRADO E DO PROFANO</h5>
+    <a href="#" class="btn btn-primary">Ver mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+ <div id="images"><img src="../../Assets/img/rioeges.jpg" alt="" width="300px" height="100px"></div>
+  <div class="card-body">
+    <h5 class="card-title">ERGES: CAMINHADA SENSORIA</h5>
+    <a href="#" class="btn btn-primary">Ver mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+ <div id="images"><img src="../../Assets/img/egitania.jpg" alt="" width="300px" height="100px"></div>
+  <div class="card-body">
+    <h5 class="card-title">ILUMINANDO O PASSADO</h5>
+    <a href="#" class="btn btn-primary">Ver mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;"> 
+<div id="images"><img src="../../Assets/img/got.jpg" alt="" width="300px" height="100px"></div>
+  <div class="card-body">
+    <h5 class="card-title">LENDAS E DRAGÕES</h5>
+    <a href="#" class="btn btn-primary">Ver mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+<div id="images"><img src="../../Assets/img/egitania2.jpg" alt="" width="300px" height="100px"></div>
+  <div class="card-body">
+    <h5 class="card-title">RETRATOS DOS TEMPOS</h5>
+    <a href="#" class="btn btn-primary">Ver mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+<div id="images"><img src="../../Assets/img/btt.jpg" alt="" width="300px" height="100px"></div>
+  <div class="card-body">
+    <h5 class="card-title">PASSEIO DE BTT NAS ALDEIAS HISTÓRICAS PORTUGUESAS</h5>
+    <a href="#" class="btn btn-primary">Ver mais</a>
+  </div>
+</div>
+<div class="card" style="width: 18rem;">
+<div id="images"><img src="../../Assets/img/geomonumento.jpg" alt="" width="300px" height="100px"></div>
+  <div class="card-body">
+    <h5 class="card-title">PEDRAS COM HISTÓRIA</h5>
+    <a href="#" class="btn btn-primary">Ver mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+<div id="images"><img src="../../Assets/img/aldeiahist.jpg" alt="" width="300px" height="100px"></div>
+  <div class="card-body">
+    <h5 class="card-title">TOUR ALDEIAS HISTÓRICAS</h5>
+    <a href="#" class="btn btn-primary">Ver mais</a>
+  </div>
+
+<div class="card" style="width: 18rem;">
+<div id="images"><img src="../../Assets/img/aldeiamedie.jpg" alt="" width="300px" height="100px"></div>
+  <div class="card-body">
+    <h5 class="card-title">TOUR ALDEIAS MEDIEVAIS</h5>
+    <a href="#" class="btn btn-primary">Ver mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+<div id="images"><img src="../../Assets/img/kayak.jpg" alt="" width="300px" height="100px"></div>
+  <div class="card-body">
+    <h5 class="card-title">TOUR AVENTURA DE KAYAK NO RIO CÔA</h5>
+    <a href="#" class="btn btn-primary">Ver mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+<div id="images"><img src="../../Assets/img/belmonte.jpg" alt="" width="300px" height="100px"></div>
+  <div class="card-body">
+    <h5 class="card-title">TOUR BELMONTE E MUSEUS</h5>
+    <a href="#" class="btn btn-primary">Ver mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+<div id="images"><img src="../../Assets/img/covaodosconchos.jpg" alt="" width="300px" height="100px"></div>
+  <div class="card-body">
+    <h5 class="card-title">TOUR COVÃO DOS CONCHOS</h5>
+    <a href="#" class="btn btn-primary">Ver mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+<div id="images"><img src="../../Assets/img/gravurarupestre.jpg" alt="" width="300px" height="100px"></div>
+  <div class="card-body">
+    <h5 class="card-title">TOUR GRAVURAS RUPESTRES</h5>
+    <a href="#" class="btn btn-primary">Ver mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+<div id="images"><img src="../../Assets/img/miradourofaia.jpg" alt="" width="300px" height="100px"></div>
+  <div class="card-body">
+    <h5 class="card-title">TOUR MIRADOURO DA FAIA</h5>
+    <a href="#" class="btn btn-primary">Ver mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+<div id="images"><img src="../../Assets/img/pordosol.jpg" alt="" width="300px" height="100px"></div>
+  <div class="card-body">
+    <h5 class="card-title">TOUR PÔR DO SOL</h5>
+    <a href="#" class="btn btn-primary">Ver mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+<div id="images"><img src="../../Assets/img/idanha.png" alt="" width="300px" height="100px"></div>
+  <div class="card-body">
+    <h5 class="card-title">UM MANJAR DOS DEUSES</h5>
+    <a href="#" class="btn btn-primary">Ver mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+<div id="images"><img src="../../Assets/img/fosseis.jpg" alt="" width="300px" height="100px"></div>
+  <div class="card-body">
+    <h5 class="card-title">UM MAR DE FÓSSEIS</h5>
+    <a href="#" class="btn btn-primary">Ver mais</a>
+  </div>
+</div>
+
+
+    <!-- <div class="container_prog">
             <div class="box_prog">
                 <div class="info_prog">
                     <div id="images"><img src="../../Assets/img/biketour1.jpg" alt="" width="300px" height="100px"></div>
@@ -50,161 +238,161 @@ include_once __DIR__ . '../../nf/nav.php';
                 <h3>CAMINHADA PELAS ALDEIAS HISTÓRICAS – MONSANTO</h3>
                 <p>Caminhe e inspire-se nas paisagens únicas do interior de Portugal e deguste iguarias regionais como o queijo, vinho, cabrito, borrego e enchidos.</p>
             </div>
-        </div>
+        </div> -->
 
-            <div class="box_prog">
+            <!-- <div class="box_prog">
                 <div class="info_prog">
                     <div id="images"><img src="../../Assets/img/biketour3.jpg" alt="" width="300px" height="100px"></div>
                         <h3>CICLOTURISMO NAS ALDEIAS HISTÓRICAS – CASTELO RODRIGO</h3>
                         <p>Envolva-se numa experiência medieval por entre as muralhas da aldeia histórica de Castelo Rodrigo, local de excepcional interesse histórico pelos monumentos que conserva.</p>
                 </div>
-            </div>
+            </div> -->
 
-            <div class="box_prog">
+            <!-- <div class="box_prog">
                 <div class="info_prog">
                     <div id="images"><img src="../../Assets/img/biketour4.jpg" alt="" width="300px" height="100px"></div>
                     <h3>CICLOTURISMO NAS ALDEIAS HISTÓRICAS – MONSANTO</h3>
                     <p>Mergulhe numa experiência incrível que mistura aldeias medievais idílicas, conheça aquela que é distinguida como sendo a “aldeia mais portuguesa de Portugal”.</p>
                 </div>
-            </div>
+            </div> -->
 
-            <div class="box_prog">
+            <!-- <div class="box_prog">
                 <div class="info_prog">
                     <div id="images"><img src="../../Assets/img/ilhapedra.jpg" alt="" width="300px" height="100px"></div>
                     <h3>DA ILHA DA PEDRA À EGITÂNIA</h3>
                     <p>Monsanto surge imponente na paisagem clamando desde logo a sua importância e majestosidade. Esta ilha de pedra rodeada de curiosas formações rochosas possui um mundo de recantos por descobrir.</p>
                 </div>
-            </div>
+            </div> -->
 
-            <div class="box_prog">
+            <!-- <div class="box_prog">
                 <div class="info_prog">
                     <div id="images"><img src="../../Assets/img/sagradoaoprofano.jpg" alt="" width="300px" height="100px"></div>
                     <h3>DO SAGRADO E DO PROFANO</h3>
                     <p>Gosta do inesperado? Quer conhecer uma aldeia que poucos conhecem? Neste passeio a Proença-a-Velha, uma das mais antigas povoações de Portugal, percorremos os seus caminhos entre oliveiras e lugares sagrados.</p>
                 </div>
-            </div>
+            </div> -->
 
-            <div class="box_prog">
+            <!-- <div class="box_prog">
                 <div class="info_prog">
                     <div id="images"><img src="../../Assets/img/rioeges.jpg" alt="" width="300px" height="100px"></div>
                     <h3>ERGES: CAMINHADA SENSORIAL</h3>
                     <p>Quer fazer uma pausa? Temos uma experiência de tranquilidade, natureza e cultura para si: nas margens do rio Erges despertamos todos os sentidos, e saboreamos a cultura local.</p>
                 </div>
-            </div>
+            </div> -->
 
-            <div class="box_prog">
+            <!-- <div class="box_prog">
                 <div class="info_prog">
                     <div id="images"><img src="../../Assets/img/egitania.jpg" alt="" width="300px" height="100px"></div>
                     <h3>ILUMINANDO O PASSADO</h3>
                     <p>Neste passeio noturno, conheça os mistérios da antiga cidade da Egitânia, caminhando entre intrigantes vestígios ali deixados durante milénios. Esta é uma atividade para corajosos e destemidos!</p>
                 </div>
-            </div>
+            </div> -->
 
-            <div class="box_prog">
+            <!-- <div class="box_prog">
                 <div class="info_prog">
                     <div id="images"><img src="../../Assets/img/got.jpg" alt="" width="300px" height="100px"></div>
                 <h3>LENDAS E DRAGÕES</h3>
                 <p>É fã da Guerra dos Tronos ? Esta atividade é para si! Visite os misteriosos cenários da Casa do Dragão, e “mergulhe” nas lendas da aldeia histórica de Monsanto.</p>
             </div>
-        </div>
+        </div> -->
 
-        <div class="box_prog">
+        <!-- <div class="box_prog">
             <div class="info_prog">
                 <div id="images"><img src="../../Assets/img/egitania2.jpg" alt="" width="300px" height="100px"></div>
                 <h3>O RETRATO DOS TEMPOS</h3>
                 <p>Viaje no tempo. Pise caminhos percorridos por romanos, visigodos e templários. Sinta e respire História na antiga cidade de Egitânia, um local com mais de 2 mil anos, onde deuses foram adorados e diferentes povos se cruzaram.</p>
             </div>
-        </div>
+        </div> -->
 
-        <div class="box_prog">
+        <!-- <div class="box_prog">
             <div class="info_prog">
                 <div id="images"><img src="../../Assets/img/btt.jpg" alt="" width="300px" height="100px"></div>
                 <h3>PASSEIO DE BTT NAS ALDEIAS HISTÓRICAS PORTUGUESAS</h3>
                 <p>Visite as remotas Aldeias Históricas do Século XII. Passeio, pousada a pousada, em trilhas romanas e desfrutar de incríveis trilhas únicas. Os mais desafiantes passeios de BTT em Portugal!</p>
             </div>
-        </div>
-
+        </div> -->
+<!-- 
         <div class="box_prog">
             <div class="info_prog">
                 <div id="images"><img src="../../Assets/img/geomonumento.jpg" alt="" width="300px" height="100px"></div>
                 <h3>PEDRAS COM HISTÓRIA</h3>
                 <p>Envolva-se num cenário em que a Natureza e o Homem são um só. Deixe-se devorar por uma paisagem avassaladora. Toque em geomonumentos, conheça a história e estratégias de guerra que se transformaram em lendas vivas.</p>
             </div>
-        </div>
+        </div> -->
 
-        <div class="box_prog">
+        <!-- <div class="box_prog">
             <div class="info_prog">
                 <div id="images"><img src="../../Assets/img/aldeiahist.jpg" alt="" width="300px" height="100px"></div>
                 <h3>TOUR ALDEIAS HISTÓRICAS</h3>
                 <p>Tour de van pelas Aldeias Históricas, mais propriamente, por Almeida, Castelo Mendo e Castelo Rodrigo. Exemplo do cartaz a baixo:</p>
             </div>
-        </div>
+        </div> -->
 
-        <div class="box_prog">
+        <!-- <div class="box_prog">
             <div class="info_prog">
                 <div id="images"><img src="../../Assets/img/aldeiamedie.jpg" alt="" width="300px" height="100px"></div>
                 <h3>TOUR ALDEIAS MEDIEVAIS</h3>
                 <p>Guardadas durante séculos por muralhas inabaláveis, vigiadas por torres e castelos ancestrais, as suas ruas de granito foram calcorreadas por diversos povos e as suas casas viram nascer personalidades marcantes da história do país.</p>
             </div>
-        </div>
+        </div> -->
 
-        <div class="box_prog">
+        <!-- <div class="box_prog">
             <div class="info_prog">
                 <div id="images"><img src="../../Assets/img/kayak.jpg" alt="" width="300px" height="100px"></div>
                 <h3>TOUR AVENTURA DE KAYAK NO RIO CÔA</h3>
                 <p>Tour Aventura de Kayak No Rio Côa, com piquenique e visita às gravuras Rupestres do Vale do Côa.</p>
             </div>
-        </div>
+        </div> -->
 
-        <div class="box_prog">
+        <!-- <div class="box_prog">
             <div class="info_prog">
                 <div id="images"><img src="../../Assets/img/belmonte.jpg" alt="" width="300px" height="100px"></div>
                 <h3>TOUR BELMONTE E MUSEUS</h3>
                 <p>Visita a Belmonte, com passagem por Centum Cellas, pelo Museu dos Descobrimentos, Museu do azeite, Castelo de Belmonte, Igreja de Santiago e Panteão dos Cabrais e pelo Ecomuseu do Zêzere.</p>
             </div>
-        </div>
+        </div> -->
 
-        <div class="box_prog">
+        <!-- <div class="box_prog">
             <div class="info_prog">
                 <div id="images"><img src="../../Assets/img/covaodosconchos.jpg" alt="" width="300px" height="100px"></div>
                 <h3>TOUR COVÃO DOS CONCHOS</h3>
                 <p>Covão dos Conchos é uma lagoa artificial localizada no Parque Natural da Serra da Estrela, em Portugal, no território da aldeia de montanha do Sabugueiro. O Covão dos Conchos tornou-se famoso pelo vertedouro de boca do sino.</p>
             </div>
-        </div>
+        </div> -->
 
-        <div class="box_prog">
+        <!-- <div class="box_prog">
             <div class="info_prog">
                 <div id="images"><img src="../../Assets/img/gravurarupestre.jpg" alt="" width="300px" height="100px"></div>
                 <h3>TOUR GRAVURAS RUPESTRES</h3>
                 <p>Tour Diurna ou Noturna. Como uma imensa galeria ao ar livre, o Vale do Côa apresenta mais de mil rochas com manifestações rupestres, identificadas em mais de 80 sítios distintos, sendo predominantes as gravuras paleolíticas.</p>
             </div>
-        </div>
+        </div> -->
 
-        <div class="box_prog">
+        <!-- <div class="box_prog">
             <div class="info_prog">
                 <div id="images"><img src="../../Assets/img/miradourofaia.jpg" alt="" width="300px" height="100px"></div>
                 <h3>TOUR MIRADOURO DA FAIA</h3>
                 <p>Localizado na Faia, Freguesia de Vale do Côa, este Miradouro é o primeiro de vários investimentos que o Município de Pinhel pretende concretizar no âmbito do projeto “Ver e Sentir o Falcão” que integra a construção de uma Falcoaria.</p>
             </div>
-        </div>
+        </div> -->
 
-        <div class="box_prog">
+        <!-- <div class="box_prog">
             <div class="info_prog">
                 <div id="images"><img src="../../Assets/img/pordosol.jpg" alt="" width="300px" height="100px"></div>
                 <h3>TOUR PÔR DO SOL</h3>
                 <p>Desfrute de um maravilhoso pôr do sol, junto de quem mais gosta. Experiência acompanhada com espumante.</p>
             </div>
-        </div>
+        </div> -->
 
-        <div class="box_prog">
+        <!-- <div class="box_prog">
             <div class="info_prog">
                 <div id="images"><img src="../../Assets/img/idanha.png" alt="" width="300px" height="100px"></div>
                 <h3>UM MANJAR DOS DEUSES</h3>
                 <p>Vamos saborear o passado! Nesta atividade em Idanha -a- Velha, trilhamos caminhos percorridos por romanos, suevos, visigodos e templários e deliciamo-nos com a cozinha de autor da Chef Maria Caldeira de Sousa na Casa da Velha Fonte.</p>
             </div>
-        </div>
+        </div> -->
 
-        <div class="box_prog">
+        <!-- <div class="box_prog">
             <div class="info_prog">
                 <div id="images"><img src="../../Assets/img/fosseis.jpg" alt="" width="300px" height="100px"></div>
                 <h3>UM MAR DE FÓSSEIS</h3>
@@ -213,7 +401,7 @@ include_once __DIR__ . '../../nf/nav.php';
         </div>
 
     </div>
-</div>
+</div> -->
 <?php
 require_once __DIR__ . '/../nf/footer.php';
 ?>
