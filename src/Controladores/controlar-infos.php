2c137568a4add23dd769ad48c170f516fe0b80e0
Andre Carvalho <andre.filipe.carvalho@outlook.pt>
2023-06-15 16:32:09 +0100

    .

--- src/Controladores/controlar-infos.php
+++ src/Controladores/controlar-infos.php
@@ -53,7 +53,7 @@ if (isset($_GET['info'])) {
         $info = lerinfo($_GET['id']); 
 
         # DELETA info
-        $sucesso = deletarinfo($info);
+        $sucesso = deletarinfo($info['id']);
 
      # REDIRECIONA UTILIZADOR PARA PÁGINA ADMIN COM MENSAGEM DE SUCCESO
      if ($sucesso) {
