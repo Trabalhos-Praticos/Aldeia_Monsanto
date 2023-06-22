0771f0b399a21d72ed913efa3dead8712aaf3f92
Andre Carvalho <andre.filipe.carvalho@outlook.pt>
2023-06-22 17:48:24 +0100

    .

--- src/Pages/Visitar/Visitar_p.php
+++ src/Pages/Visitar/Visitar_p.php
@@ -1,7 +1,7 @@
 <?php
 # CARREGA MIDDLEWARE PAGARA GARANTIR QUE APENAS UTILIZADORES ACESSE O SITIO
 require_once __DIR__ . '/../../middleware/middleware-utilizador.php';
-
+$info=lerinfosMonumentos();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -25,5 +25,5 @@ require_once __DIR__ . '/../nf/footer.php';
 ?>
 <script src="https://kit.fontawesome.com/f3e7e2778c.js" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-</body>-->
+</body>
 </html>
