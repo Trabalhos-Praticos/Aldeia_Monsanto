1a9111743a502d293c27ed9bcf31e518064c4dcf
ludvikcruz <lucas.2001.cruz@gmail.com>
2023-06-23 00:57:54 +0100

    SGF

--- src/Validacao/validar-info-registo.php
+++ src/Validacao/validar-info-registo.php
@@ -1,19 +1,19 @@
 <?php
-    function validarRegistoInfo($requisicao)
-    {
-        # RETIRA ESPAÇOS VAZIOS
-        foreach ($requisicao as $key => $value) {
-            $requisicao[$key] =  trim($requisicao[$key]);
-        }
-    
-        # VALIDANDO O CAMPO NOME
-        if (empty($requisicao['nome']) || strlen($requisicao['nome']) < 3 || strlen($requisicao['nome']) >= 255) {
-            $erros['nome'] = 'O campo Nome não pode estar vazio e deve ter entre 3 e 255 caracteres.';
-        }
-        # RETORNA ERROS
-        if (isset($erros)) {
-            return ['invalido' => $erros];
-        }
-        # RETORNA UTILIZADOR VALIDADO
-        return $requisicao;
+function validarRegistoInfo($requisicao)
+{
+    # RETIRA ESPAÇOS VAZIOS
+    foreach ($requisicao as $key => $value) {
+        $requisicao[$key] =  trim($requisicao[$key]);
     }
+
+    # VALIDANDO O CAMPO NOME
+    if (empty($requisicao['nome']) || strlen($requisicao['nome']) < 3 || strlen($requisicao['nome']) >= 255) {
+        $erros['nome'] = 'O campo Nome não pode estar vazio e deve ter entre 3 e 255 caracteres.';
+    }
+    # RETORNA ERROS
+    if (isset($erros)) {
+        return ['invalido' => $erros];
+    }
+    # RETORNA UTILIZADOR VALIDADO
+    return $requisicao;
+}
