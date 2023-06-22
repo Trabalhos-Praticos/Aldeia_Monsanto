f9804682cf4d84bb66318218995a803c9d31d5a3
ludvikcruz <lucas.2001.cruz@gmail.com>
2023-06-23 00:55:50 +0100

    GHJ

--- src/Controladores/controlar-utilizador.php
+++ src/Controladores/controlar-utilizador.php
@@ -14,85 +14,85 @@ require_once __DIR__ . '../../auxiliadores/auxiliador.php';
 ##############
 
 # VERBOS POST
-## CONTROLA A ROTA PARA CRIAÇÃO E ATUALIZAÇÃO DE UM UTILIZADOR NA PÁGINA UTILIZADOR
+## CONTROLA A ROTA PARA CRIAÇÃO E ATUALIZAÇÃO DE UM UTILIZADOR NA PÁGINA UTILIZADOR.
 if (isset($_POST['utilizador'])) {
 
-    ## CONTROLA A CRIAÇÃO DE NOVOS UTILIZADORES
+    ## CONTROLA A CRIAÇÃO DE NOVOS UTILIZADORES.
     if ($_POST['utilizador'] == 'criar') {
 
-        # CRIA UM UTILIZADOR
+        # CRIA UM UTILIZADOR.
         criar($_POST);
     }
 
-    ## CONTROLA A ATUALIZAÇÃO DE DADOS DOS UTILIZADORES
+    ## CONTROLA A ATUALIZAÇÃO DE DADOS DOS UTILIZADORES.
     if ($_POST['utilizador'] == 'atualizar') {
 
-        # ATUALIZA UM UTILIZADOR
+        # ATUALIZA UM UTILIZADOR.
         atualizar($_POST);
     }
 
-    ## CONTROLA A ATUALIZAÇÃO DE DADOS DE PERFIL DOS UTILIZADORES (APLICAÇÃO)
+    ## CONTROLA A ATUALIZAÇÃO DE DADOS DE PERFIL DOS UTILIZADORES (APLICAÇÃO).
     if ($_POST['utilizador'] == 'perfil') {
-        # ATUALIZA UM UTILIZADOR
+        # ATUALIZA UM UTILIZADOR.
         atualizarPerfil($_POST);
     }
 
-    ## CONTROLA A ATUALIZAÇÃO DA PALAVRA PASSE DE UTILIZADORES (APLICAÇÃO)
+    ## CONTROLA A ATUALIZAÇÃO DA PALAVRA PASSE DE UTILIZADORES (APLICAÇÃO).
     if ($_POST['utilizador'] == 'palavra_passe') {
 
-        # ATUALIZA UM UTILIZADOR
+        # ATUALIZA UM UTILIZADOR.
         alterarPalavraPasse($_POST);
     }
 }
 
 # VERBOS GET
-## CONTROLA A ROTA PARA O CARREGAMENTO DE UM UTILIZADOR NA PÁGINA ATUALIZAR-UTILIZADOR
+## CONTROLA A ROTA PARA O CARREGAMENTO DE UM UTILIZADOR NA PÁGINA ATUALIZAR-UTILIZADOR.
 if (isset($_GET['utilizador'])) {
 
-    ## CONTROLA A ROTA PARA A CRIAÇÃO DE NOVOS UTILIZADORES
+    ## CONTROLA A ROTA PARA A CRIAÇÃO DE NOVOS UTILIZADORES.
     if ($_GET['utilizador'] == 'atualizar') {
 
-        # RECUPERA DADOS DO UTILIZADOR PELO ID RECEBIDO
+        # RECUPERA DADOS DO UTILIZADOR PELO ID RECEBIDO.
         $utilizador = lerUtilizador($_GET['id']);
 
-        # CRIA A SESSÃO AÇÃO ATUALIZAR PARA MANIPULAR O BOTÃO DE ENVIO DO FORMULÁRIO UTILIZADOR
-        # ESSA ESTRATÉGIA FOI EXPLICADO NO FICHEIRO UTILIZADOR.PHP
+        # CRIA A SESSÃO AÇÃO ATUALIZAR PARA MANIPULAR O BOTÃO DE ENVIO DO FORMULÁRIO UTILIZADOR.
+        # ESSA ESTRATÉGIA FOI EXPLICADO NO FICHEIRO UTILIZADOR.PHP.
         $utilizador['acao'] = 'atualizar';
 
-        # ENVIA PARÂMETROS COM DADOS DO UTILIZADOR PARA A PÁGINA UTILIZADOR RECUPERAR DADOS PARA MANIPULAR A ALTERAÇÃO
+        # ENVIA PARÂMETROS COM DADOS DO UTILIZADOR PARA A PÁGINA UTILIZADOR RECUPERAR DADOS PARA MANIPULAR A ALTERAÇÃO.
         $params = '?' . http_build_query($utilizador);
 
         header('location: /src/Pages/Perfil/utilizador.php' . $params);
     }
 
-    ## CONTROLA A ROTA PARA A EXCLUSÃO DE UTILIZADORES
+    ## CONTROLA A ROTA PARA A EXCLUSÃO DE UTILIZADORES.
     if ($_GET['utilizador'] == 'deletar') {
 
-        # RECUPERA DADOS DO UTILIZADOR
+        # RECUPERA DADOS DO UTILIZADOR.
         $utilizador = lerUtilizador($_GET['id']);
 
-        # VALIDAÇÃO PARA NÃO PERMITIR DELETAR UTILIZADOR SE ELE FOR O DONO DO SISTEMA (SEGURANÇA)
+        # VALIDAÇÃO PARA NÃO PERMITIR DELETAR UTILIZADOR SE ELE FOR O DONO DO SISTEMA (SEGURANÇA).
         if ($utilizador['dono']) {
 
-            # DEFINE MENSAGEM ESPECÍFICA DE ERRO E RETORNO PARA PAINEL DE ADM
+            # DEFINE MENSAGEM ESPECÍFICA DE ERRO E RETORNO PARA PAINEL DE ADM.
             $_SESSION['erros'] = ['Este utilizador é proprietário do sistema e não pode ser apagado.'];
 
-            # REDIRECIONA UTILIZADOR COM DADOS DO FORMULÁRIO ANTERIORMENTE PREENCHIDO
+            # REDIRECIONA UTILIZADOR COM DADOS DO FORMULÁRIO ANTERIORMENTE PREENCHIDO.
             header('location: /../admin');
 
-            # RETORNA FALSO PARA EVITAR QUE PROGRAMA SIGA
+            # RETORNA FALSO PARA EVITAR QUE PROGRAMA SIGA.
             return false;
         }
 
-        # DELETA UTILIZADOR
+        # DELETA UTILIZADOR.
         $sucesso = deletar($utilizador);
 
-        # REDIRECIONA UTILIZADOR PARA PÁGINA ADMIN COM MENSAGEM DE SUCCESO
+        # REDIRECIONA UTILIZADOR PARA PÁGINA ADMIN COM MENSAGEM DE SUCCESO.
         if ($sucesso) {
-            # DEFINE MENSAGEM DE SUCESSO
+            # DEFINE MENSAGEM DE SUCESSO.
             $_SESSION['sucesso'] = 'Utilizador deletado com sucesso!';
 
-            # REDIRECIONA UTILIZADOR COM DADOS DO FORMULÁRIO ANTERIORMENTE PREENCHIDO
+            # REDIRECIONA UTILIZADOR COM DADOS DO FORMULÁRIO ANTERIORMENTE PREENCHIDO.
             header('location: /src/Pages/Admin/index.php');
         }
     }
@@ -103,107 +103,107 @@ if (isset($_GET['utilizador'])) {
 ###############
 
 /**
- * FUNÇÃO RESPONSÁVEL POR CRIAR UM NOVO UTILIZADOR
+ * FUNÇÃO RESPONSÁVEL POR CRIAR UM NOVO UTILIZADOR.
  */
 function criar($requisicao)
 {
-    # VALIDA DADOS DO UTILIZADOR. FICHEIRO VALIDAÇÃO->APLICAÇAO->ADMIN->VALIDAR-UTILIZADOR.PHP
+    # VALIDA DADOS DO UTILIZADOR. FICHEIRO VALIDAÇÃO->APLICAÇAO->ADMIN->VALIDAR-UTILIZADOR.PHP.
     $dados = utilizadorValido($requisicao);
 
-    # VERIFICA SE EXISTEM ERROS DE VALIDAÇÃO
+    # VERIFICA SE EXISTEM ERROS DE VALIDAÇÃO.
     if (isset($dados['invalido'])) {
 
-        # RECUPERA MENSAGEM DE ERRO, CASO EXISTA, E COLOCA EM SESSÃO PARA RECUPERANÃO NO FORMULARIO UTILIZADOR
+        # RECUPERA MENSAGEM DE ERRO, CASO EXISTA, E COLOCA EM SESSÃO PARA RECUPERANÃO NO FORMULARIO UTILIZADOR.
         $_SESSION['erros'] = $dados['invalido'];
 
-        # RECUPERA DADOS DO FORMULÁRIO PARA RECUPERAR PREENCHIMENTO ANTERIOR
+        # RECUPERA DADOS DO FORMULÁRIO PARA RECUPERAR PREENCHIMENTO ANTERIOR.
         $params = '?' . http_build_query($requisicao);
 
-        # REDIRECIONA UTILIZADOR COM DADOS DO FORMULÁRIO ANTERIORMENTE PREENCHIDO
+        # REDIRECIONA UTILIZADOR COM DADOS DO FORMULÁRIO ANTERIORMENTE PREENCHIDO.
         header('location: /src/Pages/Perfil/utilizador.php' . $params);
 
         return false;
     }
 
-    # GARDA FOTO EM DIRETÓRIO LOCAL (FUNÇÃO LOCAL)
+    # GARDA FOTO EM DIRETÓRIO LOCAL (FUNÇÃO LOCAL).
     $dados = guardaFoto($dados);
 
-    # GUARDA UTILIZADOR NA BASE DE DADOS (REPOSITÓRIO PDO)
+    # GUARDA UTILIZADOR NA BASE DE DADOS (REPOSITÓRIO PDO).
     $sucesso = criarUtilizador($dados);
 
-    # REDIRECIONA UTILIZADOR PARA PÁGINA DE REGISTO COM MENSAGEM DE SUCCESO
+    # REDIRECIONA UTILIZADOR PARA PÁGINA DE REGISTO COM MENSAGEM DE SUCCESO.
     if ($sucesso) {
 
-        # DEFINE MENSAGEM DE SUCESSO
+        # DEFINE MENSAGEM DE SUCESSO.
         $_SESSION['sucesso'] = 'Utilizador criado com sucesso!';
 
-        # REDIRECIONA O UTILIZADO PARA A PÁGINA ADMIN
+        # REDIRECIONA O UTILIZADO PARA A PÁGINA ADMIN.
         header('location: /src/Pages/Admin/index.php');
     }
 }
 
 /**
- * FUNÇÃO RESPONSÁVEL POR ATUALIZAR UM UTILIZADOR
+ * FUNÇÃO RESPONSÁVEL POR ATUALIZAR UM UTILIZADOR.
  */
 function atualizar($requisicao)
 {
-    # VALIDA DADOS DO UTILIZADOR
+    # VALIDA DADOS DO UTILIZADOR.
     $dados = utilizadorValido($requisicao);
 
-    # VERIFICA SE EXISTEM ERROS DE VALIDAÇÃO
+    # VERIFICA SE EXISTEM ERROS DE VALIDAÇÃO.
     if (isset($dados['invalido'])) {
 
-        # RECUPERA MENSAGEM DE ERRO, CASO EXISTA
+        # RECUPERA MENSAGEM DE ERRO, CASO EXISTA.
         $_SESSION['erros'] = $dados['invalido'];
 
-        # CRIA A SESSÃO AÇÃO ATUALIZAR PARA MANIPULAR O BOTÃO DE ENVIO DO FORMULÁRIO UTILIZADOR
+        # CRIA A SESSÃO AÇÃO ATUALIZAR PARA MANIPULAR O BOTÃO DE ENVIO DO FORMULÁRIO UTILIZADOR.
         $_SESSION['acao'] = 'atualizar';
 
-        # RECUPERA DADOS DO FORMULÁRIO PARA RECUPERAR PREENCHIMENTO ANTERIOR
+        # RECUPERA DADOS DO FORMULÁRIO PARA RECUPERAR PREENCHIMENTO ANTERIOR.
         $params = '?' . http_build_query($requisicao);
 
-        # REDIRECIONA UTILIZADOR COM DADOS DO FORMULÁRIO ANTERIORMENTE PREENCHIDO
+        # REDIRECIONA UTILIZADOR COM DADOS DO FORMULÁRIO ANTERIORMENTE PREENCHIDO.
         header('location: /src/Pages/Perfil/utilizador.php' . $params);
 
         return false;
     }
 
-    # RECUPERA DADOS DO UTILIZADOR
+    # RECUPERA DADOS DO UTILIZADOR.
     $utilizador = lerUtilizador($dados['id']);
 
-    # VALIDAÇÃO PARA NÃO PERMITIR DELETAR UTILIZADOR SE ELE FOR O DONO DO SISTEMA
+    # VALIDAÇÃO PARA NÃO PERMITIR DELETAR UTILIZADOR SE ELE FOR O DONO DO SISTEMA.
     if ($utilizador['dono'] && $dados['administrador'] == false) {
 
-        # DEFINE MENSAGEM ESPECÍFICA DE ERRO E RETORNO PARA PAINEL DE ADM
+        # DEFINE MENSAGEM ESPECÍFICA DE ERRO E RETORNO PARA PAINEL DE ADM.
         $_SESSION['erros'] = ['Este utilizador é proprietário do sistema e não pode deixar de ser administrador.'];
 
-        # REDIRECIONA UTILIZADOR COM DADOS DO FORMULÁRIO ANTERIORMENTE PREENCHIDO
+        # REDIRECIONA UTILIZADOR COM DADOS DO FORMULÁRIO ANTERIORMENTE PREENCHIDO.
         header('location: /../admin');
 
         return false;
     }
 
-    # GARDA FOTO EM DIRETÓRIO LOCAL E APAGA A FOTO ANTIGA ORIUNDA DA REQUISIÇÃO (FUNÇÃO LOCAL)
+    # GARDA FOTO EM DIRETÓRIO LOCAL E APAGA A FOTO ANTIGA ORIUNDA DA REQUISIÇÃO (FUNÇÃO LOCAL).
     if (!empty($_FILES['foto']['name'])) {
         $dados = guardaFoto($dados, $requisicao);
     }
 
-    # ATUALIZA UTILIZADOR (REPOSITÓRIO PDO)
+    # ATUALIZA UTILIZADOR (REPOSITÓRIO PDO).
     $sucesso = atualizarUtilizador($dados);
 
-    # REDIRECIONA UTILIZADOR PARA PÁGINA DE ALTERAÇÃO COM MENSAGEM DE SUCCESO
+    # REDIRECIONA UTILIZADOR PARA PÁGINA DE ALTERAÇÃO COM MENSAGEM DE SUCCESO.
     if ($sucesso) {
 
-        # DEFINE MENSAGEM DE SUCESSO
+        # DEFINE MENSAGEM DE SUCESSO.
         $_SESSION['sucesso'] = 'Utilizador alterado com sucesso!';
 
-        # DEFINI BOTÃO DE ENVIO DO FORMULÁRIO
+        # DEFINI BOTÃO DE ENVIO DO FORMULÁRIO.
         $dados['acao'] = 'atualizar';
 
-        # RECUPERA DADOS DO FORMULÁRIO PARA RECUPERAR PREENCHIMENTO ANTERIOR
+        # RECUPERA DADOS DO FORMULÁRIO PARA RECUPERAR PREENCHIMENTO ANTERIOR.
         $params = '?' . http_build_query($dados);
 
-        # REDIRECIONA UTILIZADOR COM DADOS DO FORMULÁRIO ANTERIORMENTE PREENCHIDO
+        # REDIRECIONA UTILIZADOR COM DADOS DO FORMULÁRIO ANTERIORMENTE PREENCHIDO.
         header('location: /src/Pages/Perfil/utilizador.php' . $params);
     }
 }
